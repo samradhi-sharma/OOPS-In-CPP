aad54a7755afa1ab40731a0bdccfa01282e5099c
Samradhi <samradhisharma36@gmail.com>
2025-03-27 14:40:34 +0530

    files added

---
+++ Pillars/Constructor/tempCodeRunnerFile.cpp
@@ -0,0 +1,16 @@
+// Parameterized constructor
+
+#include<iostream>
+using namespace std;
+class A{
+    public:
+    int val;
+    A(int x){
+        val =x;
+    }
+};
+int main(){
+    A a(10);
+    cout<<a.val;
+    cout<<"In MAin";
+}
